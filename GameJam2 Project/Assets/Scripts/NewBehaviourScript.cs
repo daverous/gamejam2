75f3cba91d01639c3e10209135b55458426f3281
David Wilson <dlw5@st-andrews.ac.uk>
2015-01-18 12:04:14 -0500

    More Cleanup and refactoring

---
+++ GameJam2 Project/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        float r = Random.value;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
